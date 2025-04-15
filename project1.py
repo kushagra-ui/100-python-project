33c5bbdac3713ae1982be204f8472b39323fac51
Kushagra Jaiswal <kjjaiswal2003@gmail.com>
2025-04-15 23:06:41 +0530

    Today is the Day 1 of my Coding Journey  .

---
+++ project1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("what is the Name of city u grew up in? \n")
+pet = input("What's your pet name? \n")
+print("Your band name would be :"+ city + " " + pet)
